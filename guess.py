f5dd65bf8ac77f1f642dc18363e663c449135d74
JoaoVictorSou <soulima.joao@gmail.com>
2022-03-02 10:38:54 -0300

    repetição de tentativas

--- guess.py
+++ guess.py
@@ -1,17 +1,20 @@
 secret_number = 43
+tried = 3
+round = 1
 
-choose = input('Digite o seu número: ')
+while (round <= tried):
+    print('Rodada {} de {}'.format(round, tried))
+    choose = input('Digite o seu número: ')
+    print('Chute do usuário:', choose)
+    choose = int(choose)
 
-print('Chute do usuário:', choose)
-
-choose = int(choose)
-
-if secret_number == choose:
-    print('Acertou!')
-elif (secret_number < choose):
-    print('Faça chutes mais baixos!')
-elif (secret_number > choose):
-    print('Faça chutes mais altos!')
-else:
-    print('Caractere inválido!')
+    if (secret_number == choose):
+        print('Acertou!')
+    elif (secret_number < choose):
+        print('Faça chutes mais baixos!')
+    elif (secret_number > choose):
+        print('Faça chutes mais altos!')
+    else:
+        print('Caractere inválido!')
+    round = round+1
 print('fim')
