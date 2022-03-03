8731c95d36de6fd48dfaeae8e12d1509569c8e1b
JoaoVictorSou <soulima.joao@gmail.com>
2022-03-02 22:03:24 -0300

    numero aleatorio

--- guess.py
+++ guess.py
@@ -1,20 +1,25 @@
-secret_number = 43
-tried = 3
-round = 1
+import random
 
-while (round <= tried):
+secret_number = round(random.randrange(1,101))
+tried = 3
+
+for round in range(1, tried+1):
     print('Rodada {} de {}'.format(round, tried))
-    choose = input('Digite o seu número: ')
+    choose = input('Digite um número entre 1 e 100: ')
     print('Chute do usuário:', choose)
     choose = int(choose)
 
+    if (choose < 1 or choose > 100):
+        print('Número fora do escopo')
+        continue
+    
     if (secret_number == choose):
         print('Acertou!')
+        break
     elif (secret_number < choose):
         print('Faça chutes mais baixos!')
     elif (secret_number > choose):
         print('Faça chutes mais altos!')
     else:
-        print('Caractere inválido!')
-    round = round+1
+         print('Caractere inválido!')
 print('fim')
