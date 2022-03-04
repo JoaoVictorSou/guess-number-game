508ee204a2f8bcd6b8e7eb15ccbd17df71625df7
JoaoVictorSou <soulima.joao@gmail.com>
2022-03-04 10:18:17 -0300

    score fracionado

--- guess.py
+++ guess.py
@@ -1,17 +1,21 @@
 import random
 
 secret_number = random.randrange(1,101)
-score = 1000
+score = 500
+element = 0
 tried = 0
 
 level = int(input('Escolha o nível de dificuldade: 1 (fácil), 2 (médio) ou 3 (difícil): '))
 
 if (level == 1):
     tried = 20
+    element = 5
 elif (level == 2):
     tried = 10
+    element = 10
 elif (level == 3):
     tried = 5
+    element = 20
 else:
     print('NÍVEL INVÁLIDO!')
 
@@ -28,17 +32,18 @@ for round in range(1, tried+1):
         continue
     
     if (secret_number == choose):
+        score += level * element + (tried - round)
         print('Acertou! YOU WIN!')
-        print('Pontuação: {}'.format(score))
+        print('Pontuação: {:.2f}'.format(score))
         break
     elif (tried == round):
         print('GAME OVER')
-        print('Pontuação: {}'.format(score))
+        print('Pontuação: {:.2f}'.format(score))
     elif (secret_number < choose):
         print('Faça chutes mais baixos!')
     elif (secret_number > choose):
         print('Faça chutes mais altos!')
     else:
          print('Caractere inválido!')
-    score -= difference
+    score -= difference*element/4
 print('fim')
